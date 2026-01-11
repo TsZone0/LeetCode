43af62cd1658e9e78aa032c7a318a354149a694b
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-01-11 15:26:40 +0530

    Time: 8 ms (70.53%), Space: 47.7 MB (65.42%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,15 @@
+class NumArray {
+     
+     private int[] pre;
+
+    public NumArray(int[] nums) {
+        pre = new int[nums.length+1];
+
+        for(int i=0; i<nums.length;i++){
+            pre[i+1] = pre[i]+nums[i];
+        }
+    }
+    public int sumRange(int left, int right) {
+        return pre[right+1]-pre[left];
+    }
+}
