1d4b64a245b7857748a2d45fbb633f9c084446f5
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-03-01 19:15:07 +0530

    Time: 1 ms (97.84%), Space: 42.8 MB (24.96%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,22 +1,16 @@
 class Solution {
     public int addDigits(int num) {
-
-        if(num == 0){
-            return 0;
+        while (num >= 10) {   // jab tak single digit na ho
+            int sum = 0;
+            
+            while (num > 0) {
+                sum += num % 10;  // last digit add karo
+                num = num / 10;   // last digit remove karo
+            }
+            
+            num = sum;  // new number ban gaya
         }
-
-        int sum = 0;
-
-        while(num > 0){
-            int res = num % 10;
-            sum = sum + res;
-            num = num / 10;
-        }
-
-        if(sum > 9){
-            return addDigits(sum);
-        }
-
-        return sum;
+        
+        return num;
     }
 }
