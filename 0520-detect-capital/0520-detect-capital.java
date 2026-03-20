1f65c2a1bebfd270c473d34d22a403ed1768fc67
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-03-20 08:51:33 +0530

    Time: 2 ms (29.93%), Space: 43.5 MB (10.94%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        
+        // Case 1: All uppercase
+        if(word.equals(word.toUpperCase())){
+            return true;
+        }
+        
+        // Case 2: All lowercase
+        if(word.equals(word.toLowerCase())){
+            return true;
+        }
+        
+        // Case 3: First letter uppercase, rest lowercase
+        if(Character.isUpperCase(word.charAt(0)) && 
+           word.substring(1).equals(word.substring(1).toLowerCase())){
+            return true;
+        }
+        
+        return false;
+    }
+}
