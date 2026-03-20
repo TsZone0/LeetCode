8837713bdf762f6ccd81c8d97c0c995f99f9a721
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-03-20 08:52:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
         
-        // Case 1: All uppercase
-        if(word.equals(word.toUpperCase())){
-            return true;
+        // First 2 letters se pattern decide karo
+        boolean isSecondUpper = word.length() > 1 && Character.isUpperCase(word.charAt(1));
+        
+        for(int i = 1; i < word.length(); i++){
+            
+            // agar pattern break hua to false
+            if(Character.isUpperCase(word.charAt(i)) != isSecondUpper){
+                return false;
+            }
         }
         
-        // Case 2: All lowercase
-        if(word.equals(word.toLowerCase())){
-            return true;
-        }
-        
-        // Case 3: First letter uppercase, rest lowercase
-        if(Character.isUpperCase(word.charAt(0)) && 
-           word.substring(1).equals(word.substring(1).toLowerCase())){
-            return true;
-        }
-        
-        return false;
+        // agar second letter uppercase hai to first bhi uppercase hona chahiye
+        return !isSecondUpper || Character.isUpperCase(word.charAt(0));
     }
 }
