ecbedcb960911dde6cdc352c179a9eb5984bd0aa
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-03-29 16:48:33 +0530

    Time: 1 ms (25.24%), Space: 42.1 MB (64.59%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int num) {
+        String binary = Integer.toBinaryString(num);
+        StringBuilder flipped = new StringBuilder();
+
+        for(char c : binary.toCharArray()) {
+            if(c == '0') flipped.append('1');
+            else flipped.append('0');
+        }
+
+        return Integer.parseInt(flipped.toString(), 2);
+    }
+}
