e4191bea6835c3fa26839cc0465c8d6c2ff7acef
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-04-20 14:02:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (15.45%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+     if(root==null){
+        return 0;
+     }
+
+     int sum = 0;
+     if(root.left!=null && root.left.left==null && root.left.right==null){
+        sum = sum + root.left.val;
+     }
+
+     sum = sum + sumOfLeftLeaves(root.left);
+     sum = sum + sumOfLeftLeaves(root.right);
+
+     return sum;
+    }
+}
