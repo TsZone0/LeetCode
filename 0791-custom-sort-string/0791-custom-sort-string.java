23a73c1ebbcee7acb04b92b3fdc61bfacc207549
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-06-14 10:14:32 +0530

    Time: 5 ms (15.65%), Space: 43 MB (52.2%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean[] used = new boolean[s.length()];
+
+        // Add characters according to order
+        for (int i = 0; i < order.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (order.charAt(i) == s.charAt(j)) {
+                    sb.append(s.charAt(j));
+                    used[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!used[i]) {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
