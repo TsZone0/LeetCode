51da2df05a56330d4e094312e251bc1ff1dc25f8
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-08-26 15:26:02 +0530

    Time: 2 ms (95.74%), Space: 45.6 MB (83.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,93 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> comb = new ArrayList<>();
-        combination(0,candidates,new ArrayList(),comb, target);
+        combination(0,candidates,new ArrayList<>(),comb,target);
         return comb;
     }
-    public void combination(int start, int[] nums,List<Integer>current, List<List<Integer>>comb,int target){
+
+    public void combination(int start,int[] nums,List<Integer>current,List<List<Integer>>comb,int target){
         if(target==0){
             comb.add(new ArrayList(current));
         }
         if(target<0){
             return;
         }
-        for(int i= start; i<nums.length; i++){
+        for(int i=start; i<nums.length; i++){
             current.add(nums[i]);
             combination(i,nums,current,comb,target-nums[i]);
             current.remove(current.size()-1);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     List<List<Integer>> comb = new ArrayList<>();
+    //     combination(0,candidates,new ArrayList(),comb, target);
+    //     return comb;
+    // }
+    // public void combination(int start, int[] nums,List<Integer>current, List<List<Integer>>comb,int target){
+    //     if(target==0){
+    //         comb.add(new ArrayList(current));
+    //     }
+    //     if(target<0){
+    //         return;
+    //     }
+    //     for(int i= start; i<nums.length; i++){
+    //         current.add(nums[i]);
+    //         combination(i,nums,current,comb,target-nums[i]);
+    //         current.remove(current.size()-1);
+    //     }
