39a759928e003da17047eb652cd38ebaea15befb
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-09-02 02:57:17 +0530

    Time: 1 ms (86.71%), Space: 44.4 MB (20.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,19 @@
+import java.util.*;
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> comb = new ArrayList<>();
-        subset(0,new ArrayList<>(),nums,comb);
-        return comb;
+    List<List<Integer>> result = new ArrayList<>();
+    subset(0,nums, new ArrayList<>(),result);
+    return result;
     }
-    public void subset(int start,List<Integer>current,int[] nums,List<List<Integer>> comb  ){
-        comb.add(new ArrayList<>(current));
-        for(int i=start; i<nums.length; i++){
-            current.add(nums[i]);
-            subset(i + 1, current, nums, comb);
-            current.remove(current.size()-1);
+
+    public void subset(int start,int[] size,ArrayList<Integer>curr,List<List<Integer>> result){
+        result.add(new ArrayList(curr));
+        for(int i= start;i<size.length; i++){
+            curr.add(size[i]);
+            subset(i+1,size,curr,result);
+            curr.remove(curr.size()-1);
         }
+
+        
     }
 }
