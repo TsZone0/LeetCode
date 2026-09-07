c27f3ec2d29f39d9dbac9369e752d4aa56dd75fd
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-09-07 18:41:25 +0530

    Time: 16 ms (88.05%), Space: 97.6 MB (95.14%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        set(1,n,k,new ArrayList<>(),result);
+        sublist(1,n,k,new ArrayList<>(),result);
         return result;
     }
-    public void set(int start,int n,int k, List<Integer>curr, List<List<Integer>>result){
+
+    public void sublist(int start, int n, int k, List<Integer> curr, List<List<Integer>> result){
         if(curr.size()==k){
-             result.add(new ArrayList(curr));
-             return;
+            result.add(new ArrayList(curr));
+            return;
         }
-       
         for(int i=start; i<=n; i++){
             curr.add(i);
-            set(i+1,n,k,curr,result);
+            sublist(i+1, n,k,curr,result);
             curr.remove(curr.size()-1);
         }
-
     }
 }
